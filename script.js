fb17938437da6063aec6fc01c035d2fe45792327
Naman Agarwal <namanagarwal9167@gmail.com>
2023-03-23 19:00:24 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let turn = "X";
+let gameover = false;
 const changeTurn = () => {
   return turn === "X" ? "O" : "X";
 };
@@ -10,7 +11,45 @@ Array.from(boxes).forEach((element) => {
     if (boxtext.innerText === "") {
       boxtext.innerText = turn;
       turn = changeTurn();
-      document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+      checkwin();
+      if (!gameover) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for " + turn;
+      }
     }
   });
+});
+
+const checkwin = () => {
+  let boxtext = document.getElementsByClassName("boxtext");
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  wins.forEach((e) => {
+    if (
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText =
+        boxtext[e[0]].innerText + " Won";
+      gameover = true;
+    }
+  });
+};
+reset.addEventListener("click", () => {
+  let boxtext = document.querySelectorAll(".boxtext");
+  Array.from(boxtext).forEach((element) => {
+    element.innerText = "";
+  });
+  turn = "X";
+  gameover = false;
+  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
 });
